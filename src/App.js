8a9dd159603b7c54e7adc95e44a05d6b091af941
Software Developer (Full Stack & Web Development) ' React Laravel MongoDB Developer <100100319+benchakirAbdelhamid@users.noreply.github.com>
2023-09-08 15:12:58 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './pages/login';
+import SignUp from './pages/SignUp';
+import Home from './pages/home';
+import './App.css';
+import LoginAutoEcole from "./pages/LoginAutoEcole";
+import SignUpAutoEcole from "./pages/SignUpAutoEcole";
+
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/LoginAutoEcole" element={<LoginAutoEcole />} />
+          <Route path="/SignUpAutoEcole" element={<SignUpAutoEcole />} />
+          <Route path="*" element={<> not found</>} />
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/LoginAutoEcole.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginAutoEcol from '../components/LoginAutoEcole'
+const LoginAutoEcole = () => {
+    return (
+        <div className="container-body">
+        <div className="parent-form-img">
+          <LoginAutoEcol />
+        </div>
+      </div>
+    );
+}
+
+export default LoginAutoEcole;
